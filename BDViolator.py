c923dbc85d418506872988ed2aa9f7da1b685ff1
WheelsVT <WheelsVT@users.noreply.github.com>
2020-11-13 11:36:37 -0500

    Add files via upload
    Modified to handle severity and violation rules.

--- BDViolator.py
+++ BDViolator.py
@@ -1,12 +1,15 @@
 #BlackDuck-Violator: Process BlackDuck component scan to mark components as "In Violation" or "Not In Violation"
-#                   based upon designated list of license rules. This tool is intended for use as prep for import
-#                   into DetectDojo for license risk management.
+#       and set review severity/priority based upon designated list of license rules. This tool is intended for use as prep for import
+#       into DetectDojo for license risk management where results may not match up to self-determined
+#       license risk rules.
 
-# Process: unzip BlackDuck component data, stream components.csv to new file replacing violation field based upon
-#          matches within the RiskyLicenseList.csv file.
+# Process: unzip BlackDuck component data, stream components.csv to new file replacing violation and severity field based upon
+#       matches within the RiskyLicenseList.csv file.
 
 # RiskyLicenseList TIP: use the UID and License name from known BlackDuck scan results for exact match.
-#                       If unknown, use a UID of '0' and text to match for a best guess approach.
+#       If unknown, use a UID of 'FFFFFFFFFFFFFFFF' and text to match for a best guess approach.
+#       00000000-0010-0000-0000-000000000000, Unknown License, In Violation  <-- will create violation for unknown licenses
+#       #00000000-0010-0000-0000-000000000000, #Unknown License, HIGH <-- will set unknown licenses for review at high severity
 
 import zipfile
 import tempfile
@@ -46,18 +49,27 @@ def read_bad_list():
     for line in bad_list:
         if "License ids, License" not in line:
             pos = 0
+            items = []
+            sev = []
             for item in line.split(","):
-                if item != "0":
-                    bad_items.append(item.strip())
+                if pos == 0 or pos == 1:
+                    items.append(item.strip())
                     print("\tBad license: %s"%item.strip())
+                elif pos == 2:
+                    sev.append(item.strip())
+                    sev.append(item.strip()) #adding 2 on purpose
+                    print("\tSeverity: %s"%item.strip())
+                pos += 1
+            bad_items.append([items,sev])
     return bad_items
 
+#return severity level of the match
 def check_for_match( line,bad_list ):
     for bad in bad_list:
-        if bad in line:
-            #if "Sun" in line:
-            #    print("Found Affero: %s"%line)
-            return True
+        if bad[0][0] in line:
+            return bad[1][0]
+        if bad[0][1] in line:
+            return bad[1][1]
     return False
 
 def process( directory,bad_list ):
@@ -69,17 +81,38 @@ def process( directory,bad_list ):
             print(" Found Component CSV.")
             file = open(entry.path, "r")
             fileout = open(entry.path+"2", "w")
+            linenum = 0
             for line in file:
-                if check_for_match( line,bad_list ):
-                    #we have a bad license
-                    fileout.write(line.replace("Not In Violation","In Violation"))
-                    count += 1
-                else:
-                    #keep original but reset the violation line if needed
-                    if "Not In Violation" not in line:
-                        fileout.write(line.replace("In Violation","Not In Violation"))
+                if linenum != 0:
+                    sev = check_for_match( line, bad_list )#either match and set severity or False for no match
+                    #process the line to adjust the new severity setting
+                    if sev and "Violation" in sev:#this is an  "In Violation" issue, not a license risk for "Review"
+                        fileout.write(line.replace("Not In Violation","In Violation"))
+                        count += 1
                     else:
-                        fileout.write(line)
+                        pos = 0
+                        for item in line.split(","):
+                            if pos < 20 or ("OK" not in item and "HIGH" not in item and "MEDIUM" not in item and "LOW" not in item):
+                                if item != "\n":
+                                    if "Violation" in item:#make sure we clear any violations outside of our custom
+                                        fileout.write("Not In Violation,")
+                                    else:
+                                        fileout.write(item + ",")#write the normal field
+                                else:
+                                    fileout.write(item)
+                            else:#on the "License Risk" field
+                                #print("%s - %s"%(item,pos))
+                                if sev:
+                                    count += 1
+                                    #print("%s - %s"%(item,pos))
+                                    fileout.write(sev + ",")#write our custom severity
+                                else:
+                                    fileout.write("OK,")#clear the license risk
+                            pos += 1
+                else:
+                    fileout.write(line)
+                linenum += 1
+            fileout.close()
             os.replace(entry.path+"2",entry.path)
     if found:
         print(" Processed Archive. Found %s component violations."%count)
